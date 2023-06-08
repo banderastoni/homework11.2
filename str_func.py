8e3a6c62e4282077ead4d6f8762e0dfe5e90f86a
banderastoni <banderastoni@rambler.ru>
2023-06-08 14:15:50 +0300

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -1,8 +1,1 @@
 def string_to_upper(string):
-    """Возвращает строку со всеми заглавными буквами"""
-    return string.upper()
-
-
-def string_to_title(string):
-    """Возвращает строку с заглавными первыми буквами в каждом слове"""
-    return string.title()
