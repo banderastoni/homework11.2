b78fb2dc50c913f2c69add91afcb5ee2a163cab0
banderastoni <banderastoni@rambler.ru>
2023-06-08 14:17:55 +0300

    feature/Task3 add 2nd func + docstring

--- str_func.py
+++ str_func.py
@@ -1,1 +1,7 @@
 def string_to_upper(string):
+    return string.upper()
+
+
+def string_to_title(string):
+    """Возвращает строку с заглавными первыми буквами в каждом слове"""
+    return string.title()
